1d68d1b2785337dedb2fd4fbe92d443d574aa0d3
Paradis <49301263+Paradis4432@users.noreply.github.com>
2022-11-29 18:35:13 -0300

    Update autoType.js

--- js/autoType.js
+++ js/autoType.js
@@ -1,5 +1,5 @@
 var options = {
-    strings: ['Paradis', 'a BackEnd Developer', 'a Web Developer', 'a Data Scienctist'],
+    strings: ['Paradis', 'a BackEnd Developer', 'a Web Developer', 'a Data Scientist'],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
